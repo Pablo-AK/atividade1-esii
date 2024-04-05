c1005824168fd16a55af4b33a268ee0ebd0ed5e1
pablokonrath <pabloak@live.com>
2024-04-04 21:44:43 -0300

    funcao de soma criada

--- calculator.js
+++ calculator.js
@@ -1,6 +1,10 @@
 // retorna o valor da soma entre os dois parâmetros
-function sum(v1, v2) {}
+function sum(v1, v2) {
+  return v1 + v2;
+}
+console.log(sum(2, 2));
 
+/*
 // retorna o valor da subtração entre os dois parâmetros
 function sub(v1, v2) {}
 
@@ -12,5 +16,4 @@ function mult(v1, v2) {}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
-
-console.log(sum(v1, v2));
+*/
