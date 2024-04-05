0d8888aa497438c33abb885b01b67bbb310aca35
Ricardo Botelho da Silva <rsbotelho.41@gmail.com>
2024-04-04 21:53:09 -0300

    funcao da raiz quadrada

--- calculator.js
+++ calculator.js
@@ -11,6 +11,15 @@ function div(v1, v2) {}
 function mult(v1, v2) {}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
+function square(v1) {
+    return Math.sqrt(v1)
+}
+
+// Exemplo de uso da função da raiz
+
+const raiz = Math.sqrt(25);
+console.log(raiz); // Output: 5
 
 console.log(sum(v1, v2));
+
+
