4d31453931bce9f29d67e89f1ae1b9c371e8051b
Felipe <felipezundlercontato@gmail.com>
2024-04-11 19:40:46 -0300

    função de subtraçao

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ## 🔐 Pré requisitos
 
 <a href="https://nodejs.dev/">Node</a> &nbsp;
+<a href="https://www.python.org/">Python</a> &nbsp;
+<a href="https://www.php.net/">PHP</a> &nbsp;
+<a href="https://www.typescriptlang.org/">TypeScript</a> &nbsp;
 
 ## Instalação do Projeto
 
@@ -18,4 +21,5 @@ $ node calculator.js
 ## 🤝 Contribuídores
 
 <a href="https://github.com/felipehaertelsenac"><img src="https://github.com/felipehaertelsenac.png" width="45" height="45"></a> &nbsp;
+<a href="https://github.com/ricardobotelho"><img src="https://github.com/ricardobotelho.png" width="45" height="45"></a> &nbsp;
 
--- calculator.js
+++ calculator.js
@@ -1,9 +1,18 @@
 // retorna o valor da soma entre os dois parâmetros
-function sum(v1, v2) {}
+function sum(v1, v2) {
+  return v1 + v2;
+}
+console.log(sum(10, 10));
 
 // retorna o valor da subtração entre os dois parâmetros
-function sub(v1, v2) {}
+function sub(v1, v2) {// Importa a função de subtração do arquivo subtracao.js
 
+  // Exemplo de uso da função de subtração
+  return v1 - v2
+     
+  ;}
+
+  console.log(sub(10, 5))
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {}
 
@@ -11,6 +20,12 @@ function div(v1, v2) {}
 function mult(v1, v2) {}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
-
-console.log(sum(v1, v2));
+function square(v1) {
+  return Math.sqrt(v1);
+}
+console.log(square(25));
+// Exemplo de uso da função da raiz
+/*
+const raiz = Math.sqrt(25);
+console.log(raiz); // Output: 5
+*/
